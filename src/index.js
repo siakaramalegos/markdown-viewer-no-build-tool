cf493b71e734078d2b11288e412883aa48e80315
Sia Karamalegos <siakaramalegos@gmail.com>
2020-05-25 09:31:19 -0500

    Remove webpack and lodash

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import { initializeMarkdown } from './markdown';
-
-document.getElementById('md-button').onclick = function(e) {
-  e.target.className = 'hide'
-  document.getElementById('markdown-container').className = 'show'
-  initializeMarkdown()
-}
